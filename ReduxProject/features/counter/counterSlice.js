2804ed3badbb1791479e06f849e2372dfd17b812
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-10-02 21:59:30 +0530

    ...

---
+++ ReduxProject/features/counter/counterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+    reset: (state) => state=0,
+  },
+})
+export default counterSlice.reducer
+export const{
+  increment,decrement,reset
+}=counterSlice.actions
---
+++ ReduxProject/src/App.jsx
@@ -0,0 +1,12 @@
+
+// import store from '../App/store'
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    </>
+  )
+}
+
+export default App
